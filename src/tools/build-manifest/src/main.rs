6627cbbad70fbe9a5d262dbb07d33d1b8fb1c51e
Pietro Albini <pietro@pietroalbini.org>
2020-09-29 15:33:25 +0200

    bootstrap: always use the Rust version in package names
    The format of the tarballs produced by CI is roughly the following:
    
        {component}-{release}-{target}.{ext}
    
    While on the beta and nightly channels `{release}` is just the channel
    name, on the stable channel is either the Rust version or the version of
    the component we're shipping:
    
        cargo-0.47.0-x86_64-unknown-linux-gnu.tar.xz
        clippy-0.0.212-x86_64-unknown-linux-gnu.tar.xz
        llvm-tools-1.46.0-x86_64-unknown-linux-gnu.tar.xz
        miri-0.1.0-x86_64-unknown-linux-gnu.tar.xz
        rls-1.41.0-x86_64-unknown-linux-gnu.tar.xz
        rust-1.46.0-x86_64-unknown-linux-gnu.tar.xz
        ...
    
    This makes it really hard to get the package URL without having access
    to the manifest (and there is no manifest on ci-artifacts.rlo), as there
    is no consistent version number to use.
    
    This commit addresses the problem by always using the Rust version
    number as `{release}` for the stable channel, regardless of the version
    number of the component we're shipping. I chose that instead of "stable"
    to avoid breaking the URL scheme *that* much.
    
    Rustup should not be affected by this change, as it fetches the URLs
    from the manifest. Unfortunately we don't have a way to test other
    clients before making a stable release, as this change only affects the
    stable channel.

--- src/tools/build-manifest/src/main.rs
+++ src/tools/build-manifest/src/main.rs
@@ -289,7 +289,7 @@ impl Builder {
         self.digest_and_sign();
         let manifest = self.build_manifest();
 
-        let rust_version = self.versions.package_version(&PkgType::Rust).unwrap();
+        let rust_version = self.versions.rustc_version();
         self.write_channel_files(self.versions.channel(), &manifest);
         if self.versions.channel() != rust_version {
             self.write_channel_files(&rust_version, &manifest);
